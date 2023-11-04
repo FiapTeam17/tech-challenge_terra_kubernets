5022bb3009f2f6d4531ad2ddb257404dbd1e71f5
GarciaGP <gabriel.garp@outlook.com>
2023-11-03 23:23:08 -0300

    update cpu

--- ecs.tf
+++ ecs.tf
@@ -76,7 +76,7 @@ resource "aws_ecs_task_definition" "tech-challenge-task" {
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   execution_role_arn       = aws_iam_role.ecs_execution_role.arn
-  cpu                      = 0.5
+  cpu                      = 1
   memory                   = 512
 
 
