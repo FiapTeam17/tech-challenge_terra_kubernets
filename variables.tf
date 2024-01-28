677d2cd52372cc286595d46fe5f23b43b6477ffc
GarciaGP <gabriel.garp@outlook.com>
2024-01-28 20:10:28 -0300

    exclui variaveis nao usadas e configura actions para serem executadas na main

--- variables.tf
+++ variables.tf
@@ -7,27 +7,3 @@ variable "mongodb_connection_string" {
   type      = string
   sensitive = true
 }
-
-variable "ecs_containers" {
-  description = "Propriedades para as imagens de cada microsserviço"
-  type = map(object({
-    image     = string
-    db_host   = string
-    db_schema = string
-  }))
-
-
-  default = {
-    "pedido_service" = {
-      image     = "258775715661.dkr.ecr.us-east-2.amazonaws.com/sgr-service-pedido"
-      db_host   = "sgr-rds-instance-pedido.c5c6gu62ikas.us-east-2.rds.amazonaws.com"
-      db_schema = "sgr_database_pedido"
-    },
-    "producao_service" = {
-      image     = "258775715661.dkr.ecr.us-east-2.amazonaws.com/sgr-service-producao"
-      db_host   = "sgr-rds-instance-producao.c5c6gu62ikas.us-east-2.rds.amazonaws.com"
-      db_schema = "sgr_database_producao"
-    }
-  }
-
-}
